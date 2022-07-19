085bfe102d082c725a624cdfc05c20f44a850be7
Marc Jordà <mjorda@ac.upc.edu>
2022-07-19 14:44:40 +0200

    Fix flag, only export flexmalloc envars when needed

--- scripts/run.sh
+++ scripts/run.sh
@@ -75,25 +75,14 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-#output_file=${arg_output_file:-$ECOHMEM_ADVISOR_OUTPUT_FILE}
-
 fm_mem_config=${arg_fm_config:-$ECOHMEM_FLEXMALLOC_MEM_CONFIG}
 obj_dist_file=${arg_obj_file:-$ECOHMEM_ADVISOR_OUTPUT_FILE}
 
 app_args_str=${arg_app_args:-$ECOHMEM_APP_ARGS}
 mpirun_flags_str=${arg_mpirun_flags:-$ECOHMEM_MPIRUN_FLAGS}
 app_runner=${arg_app_runner:-$ECOHMEM_APP_RUNNER}
-app_runner_flags_str=${arg_app_runner_flags:-$ECOHMEM_APP_RUNNER_FLAGS}
+app_runner_flags_str=${arg_runner_flags:-$ECOHMEM_APP_RUNNER_FLAGS}
 
-
-#output_dir=
-#app_out_file=$output_dir/run.out
-#app_err_file=$output_dir/run.err
-
-export FLEXMALLOC_HOME=$ECOHMEM_FLEXMALLOC_HOME
-export FLEXMALLOC_FALLBACK_ALLOCATOR=$ECOHMEM_FLEXMALLOC_FALLBACK_ALLOCATOR
-export FLEXMALLOC_MINSIZE_THRESHOLD=$ECOHMEM_FLEXMALLOC_MINSIZE_THRESHOLD
-export FLEXMALLOC_MINSIZE_THRESHOLD_ALLOCATOR=$ECOHMEM_FLEXMALLOC_MINSIZE_THRESHOLD_ALLOCATOR
 
 # parse quotes and backslashes in arg/flag lists
 app_runner_flags=()
@@ -117,6 +106,11 @@ else
 fi
 
 if [[ $arg_load_fm -eq 1 ]]; then
+    export FLEXMALLOC_HOME=$ECOHMEM_FLEXMALLOC_HOME
+    export FLEXMALLOC_FALLBACK_ALLOCATOR=$ECOHMEM_FLEXMALLOC_FALLBACK_ALLOCATOR
+    export FLEXMALLOC_MINSIZE_THRESHOLD=$ECOHMEM_FLEXMALLOC_MINSIZE_THRESHOLD
+    export FLEXMALLOC_MINSIZE_THRESHOLD_ALLOCATOR=$ECOHMEM_FLEXMALLOC_MINSIZE_THRESHOLD_ALLOCATOR
+
     cmd+=("$ECOHMEM_LOAD_FLEXMALLOC_SCRIPT" "$fm_mem_config" "$obj_dist_file")
 fi
 
