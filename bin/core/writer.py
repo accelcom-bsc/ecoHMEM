32aff3d38c05d37c96f75cbadbbc66c2e774fa25
Hatem Elshazly <hatem.elshazly@bsc.es>
2022-12-21 15:46:16 +0100

    Refactored writing the output step

---
+++ bin/core/writer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from abc import ABC, abstractmethod
+
+class Writer:
+    args = None
+    
+    def __init__(self, args):
+        self.args = args
+
+    @abstractmethod
+    def write(self, distribution, mem_systems):
+        pass
